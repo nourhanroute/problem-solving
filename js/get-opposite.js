45a012487647ad66288fa878bbe70faab3d56a41
nourhanroute <nourhanroute@gmail.com>
2024-05-24 21:38:03 +0300

    Create get-opposite.js

---
+++ js/get-opposite.js
@@ -0,0 +1,25 @@
+
+// ^ Very simple, given an integer or a floating-point number, find its opposite.
+
+// * Examples:
+// * 1: -1
+// * 14: -14
+// * -34: 34
+
+
+function getOpposite(num){
+
+    // return num * -1;
+
+    return - num ;
+
+}
+console.log(getOpposite(7));
+
+
+
+
+
+
+
+
